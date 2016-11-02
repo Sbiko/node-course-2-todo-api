a49808dba58dee4d1bdb2320694bb205c9607433
youness <sbikowak@gmail.com>
2016-11-02 17:57:59 +0000

    test change mongoi

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp/');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp/');// //
+mongoose.connect(mongodb://sbiko:test123@ds141937.mlab.com:41937/todoapp');
 
 
 module.exports = {mongoose};
